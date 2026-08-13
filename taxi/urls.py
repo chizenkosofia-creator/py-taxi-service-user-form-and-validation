2192d8ee48ae722c0b46dd86d3cbd62fa99668fe
Sofia <chizenko.sofia@gmail.com>
2026-08-13 21:02:48 +0300

    solution7

--- taxi/urls.py
+++ taxi/urls.py
@@ -52,8 +52,9 @@ urlpatterns = [
     path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
     path("drivers/<int:pk>/delete/", DriverDeleteView.as_view(),
          name="driver-delete"),
-    path("drivers/<int:pk>/driver-update/", DriverLicenseUpdateView.as_view(),
-         name="driver-update-license"),
+    path(
+        "drivers/<int:pk>/update/", DriverLicenseUpdateView.as_view(),
+        name="driver-update"),
     path("cars/<int:pk>/toggle-assign/", toggle_assign_to_car,
          name="toggle-car-assign"),
 ]
